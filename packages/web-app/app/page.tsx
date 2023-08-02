89428755b695d88a3bda47ec999477e7fdfeb48c
Ovinsyah Al Bayhaqy <ovinsyah.bayhaqy@flockasia.com>
2023-08-02 10:32:39 +0700

    feat: init monorepo

---
+++ packages/web-app/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Button} from "@app/shared";
+
+function page() {
+  return (
+    <>
+      <Button />
+      <div>page</div>
+    </>
+  );
+}
+
+export default page;
---
+++ packages/web-app/next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ["@app/shared"],
+};
+
+module.exports = nextConfig;
